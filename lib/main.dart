806a23eb2de414eab61a86c61c5578f316dbe888
Andrey Lubnin <and2255@yandex.ru>
2022-01-12 16:28:19 +0300

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:themoviedb/widgets/auth/auth_widget.dart';
 import 'package:themoviedb/widgets/main_screen/main_screen_widget.dart';
@@ -22,50 +20,9 @@ class MyApp extends StatelessWidget {
               const AppBarTheme(backgroundColor: Color.fromRGBO(3, 37, 65, 1))),
       routes: {
         '/auth': (context) => const AuthWidget(),
-        // '/main_screen': (context) => const ExampleWidget(),
         '/main_screen': (context) => const MainScreenWidget(),
       },
       initialRoute: '/auth',
-      // onGenerateRoute: (RouteSettings settings) {
-      //   // используется также в случае если нет маршрута, а также если разные полномочия у пользователей.
-
-      //   return MaterialPageRoute<void>(builder: (context) {
-      //     return const Scaffold(
-      //       body: Center(
-      //         child: Text('Произошла ошибка навигации'),
-      //       ),
-      //     );
-      //   });
-      // },
     );
   }
 }
-
-// class ExampleWidget extends StatefulWidget {
-//   const ExampleWidget({Key? key}) : super(key: key);
-
-//   @override
-//   _ExampleWidgetState createState() => _ExampleWidgetState();
-// }
-
-// class _ExampleWidgetState extends State<ExampleWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('TMDB'),
-//           centerTitle: true,
-//         ),
-//         body: Center(
-//           child: ElevatedButton(
-//             onPressed: () {
-//               if (Navigator.of(context).canPop()) {
-//                 Navigator.of(context).pop;
-//               }
-//               ;
-//             },
-//             child: Text('Жми'),
-//           ),
-//         ));
-//   }
-// }
