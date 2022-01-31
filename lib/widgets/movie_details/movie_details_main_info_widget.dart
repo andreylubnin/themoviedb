9f6b1f47be69206ebfba1dc3851cc36e86c9ef8d
Andrey Lubnin <and2255@yandex.ru>
2022-01-31 14:45:05 +0300

    L41 finished

--- lib/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/widgets/movie_details/movie_details_main_info_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb/resources/resources.dart';
+import 'package:themoviedb/widgets/elements/radial_percent_widget.dart';
 
 class MovieDetailsMainInfoWidget extends StatelessWidget {
   const MovieDetailsMainInfoWidget({Key? key}) : super(key: key);
@@ -7,39 +8,132 @@ class MovieDetailsMainInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        const _TopPosterWidget(),
-        const Padding(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        _TopPosterWidget(),
+        Padding(
           padding: EdgeInsets.all(15.0),
           child: _MovieNameWidget(),
         ),
-        const SummaryWidget(),
-        const Text('From the author of Rainbow Six.'),
-        const Text('Overview'),
-        const Text(
-            'Коллеги и жена Джона Келли убиты. Чудом оставшийся в живых мужчина решает найти преступников и отомстить.'),
+        _ScoreWidget(),
+        SummaryWidget(),
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Text('From the author of Rainbow Six.',
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 17))),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: _OverviewWidget(),
+        ),
+        _DescriptionWidget(),
+        SizedBox(height: 30),
+        _PeopleWidgets(),
+      ],
+    );
+  }
+}
+
+class _PeopleWidgets extends StatelessWidget {
+  const _PeopleWidgets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const nameStyle = TextStyle(
+        color: Colors.white, fontWeight: FontWeight.bold, fontSize: 17);
+    const jobTitleStyle = TextStyle(
+        color: Colors.white, fontWeight: FontWeight.w400, fontSize: 17);
+    return Column(
+      children: [
         Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Column(
-              children: const [Text('Stefano Sollima'), Text('Director')],
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Stefano Sollima', style: nameStyle),
+                  Text('Director', style: jobTitleStyle)
+                ],
+              ),
             ),
-            Column(
-              children: const [Text('Tom Clancy'), Text('Novel')],
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Tom Clancy', style: nameStyle),
+                  Text('Novel', style: jobTitleStyle)
+                ],
+              ),
             ),
           ],
         ),
+        const SizedBox(
+          height: 20,
+        ),
         Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              children: const [Text('Taylor Sheridan'), Text('Screenplay')],
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Taylor Sheridan', style: nameStyle),
+                  Text('Screenplay', style: jobTitleStyle)
+                ],
+              ),
             ),
-            Column(
-              children: const [Text('Will Staples'), Text('Screenplay')],
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Will Staples', style: nameStyle),
+                  Text('Screenplay', style: jobTitleStyle)
+                ],
+              ),
             ),
           ],
         ),
       ],
     );
+  }
+}
+
+class _DescriptionWidget extends StatelessWidget {
+  const _DescriptionWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Text(
+          'Коллеги и жена Джона Келли убиты. Чудом оставшийся в живых мужчина решает найти преступников и отомстить.',
+          style: TextStyle(color: Colors.white, fontSize: 16)),
+    );
+  }
+}
+
+class _OverviewWidget extends StatelessWidget {
+  const _OverviewWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Обзор',
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.w600, fontSize: 17));
   }
 }
 
@@ -49,7 +143,7 @@ class _TopPosterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
+      children: const [
         Image(
           image: AssetImage(AppImages.topHeader),
         ),
@@ -71,7 +165,7 @@ class _MovieNameWidget extends StatelessWidget {
     return RichText(
       maxLines: 3,
       textAlign: TextAlign.center,
-      text: TextSpan(children: [
+      text: const TextSpan(children: [
         TextSpan(
             text: 'Tom Clancy`s Without Remorse',
             style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17)),
@@ -83,16 +177,59 @@ class _MovieNameWidget extends StatelessWidget {
   }
 }
 
+class _ScoreWidget extends StatelessWidget {
+  const _ScoreWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Row(
+            children: const [
+              SizedBox(
+                width: 45,
+                height: 45,
+                child: RadialPercentWidget(
+                    percent: 0.72,
+                    fillColor: Color.fromARGB(255, 10, 23, 25),
+                    freeColor: Color.fromARGB(255, 37, 203, 103),
+                    lineColor: Color.fromARGB(255, 25, 54, 31),
+                    lineWidth: 3,
+                    child: Text('72', style: TextStyle(color: Colors.white))),
+              ),
+              SizedBox(width: 10),
+              Text('User Score'),
+            ],
+          ),
+        ),
+        Container(width: 1.0, height: 15, color: Colors.grey),
+        TextButton(
+          onPressed: () {},
+          child: Row(
+            children: const [
+              Icon(Icons.play_arrow),
+              Text('Play Trailer'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class SummaryWidget extends StatelessWidget {
   const SummaryWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
+    return const ColoredBox(
       color: Color.fromRGBO(22, 21, 25, 1.0),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 10.0),
-        child: const Text(
+        child: Text(
           'R, 29/04/2021 (US), 1h 49m, боевик, триллер',
           maxLines: 3,
           textAlign: TextAlign.center,
